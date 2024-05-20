9d8a2d6f5f061d6760a9b030f3a2d1092eb77a20
Pure <hayubiw@gmail.com>
2024-05-21 00:19:55 +0200

    s

---
+++ chickens.js
@@ -0,0 +1,30 @@
+const chickens = [
+    {
+        "copyright": {
+            "name": "chicken lessons",
+            "author": "Anonymous",
+            "authorLink": "https://poly.pizza/u/Anonymous",
+            "license": "[CC-BY]",
+            "licenseLink": "https://creativecommons.org/licenses/by/3.0/",
+            "source": "https://poly.pizza/m/2tjmDzIm_Yw",
+        },
+        "path": "leo",
+        "name": "Leo",
+        "description": "Meet Leo, the crown jewel of The Chicken Markt. Leo is not just a chicken; he is an extraordinary companion with a level of intelligence that surpasses all others in the avian world. Raised with exceptional care and trained to the highest standards, Leo demonstrates remarkable problem-solving skills and interactive communication abilities. His superior intellect is matched by his gentle and sociable nature, making him the perfect addition to any home. Experience the unique joy of adopting Leo, and enjoy the unparalleled companionship that only the smartest chicken can provide.",
+        "imageCount": 4
+    },
+    {
+        "copyright": {
+            "name": "Chicken",
+            "author": "jeremy",
+            "authorLink": "https://poly.pizza/u/jeremy",
+            "license": "[CC-BY]",
+            "licenseLink": "https://creativecommons.org/licenses/by/3.0/",
+            "source": "https://poly.pizza/m/1YE8U35HXsI",
+        },
+        "path": "cluck_norris",
+        "name": "Cluck Norris",
+        "description": "Cluck Norris is a chicken with a mission. He is a master of martial arts and a legend in the world of poultry. Cluck Norris has dedicated his life to protecting the innocent and fighting for justice. With his lightning-fast reflexes and unbeatable strength, Cluck Norris is a force to be reckoned with. He is a true hero and a symbol of hope for chickens everywhere. Adopt Cluck Norris today and join him in his quest to make the world a better place.",
+        "imageCount": 3
+    }
+];
