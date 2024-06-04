7ec559b84d6ff76c2b4672f0cf166e9f39d95e8b
Pure <hayubiw@gmail.com>
2024-06-05 00:49:33 +0200

    s

--- scripts/three.js
+++ scripts/three.js
@@ -12,12 +12,16 @@ function render3D(model, container) {
     let pauseTimeout;
 
     const scene = new THREE.Scene();
+    //scene.background = new THREE.Color(0x1d81d8);
     const camera = new THREE.PerspectiveCamera();
     camera.position.set(15, 3, 10);
 
-    const renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setClearColor(0x000000, 0);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0x1d81d8, 1);
     renderer.setSize(container.clientWidth, container.clientWidth);
+    renderer.domElement.setAttribute("x-show", "image == 0");
+    renderer.domElement.classList.add("rounded-xl");
+
     container.appendChild(renderer.domElement);
 
     const ambientLight = new THREE.AmbientLight(0x404040);
@@ -30,6 +34,18 @@ function render3D(model, container) {
     const directionalLight2 = new THREE.DirectionalLight(0xffffff, 0.8);
     directionalLight2.position.set(-5, 5, -5);
     scene.add(directionalLight2);
+
+    const directionalLight3 = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight3.position.set(0, 5, 0);
+    scene.add(directionalLight3);
+
+    const directionalLight4 = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight4.position.set(0, 0, 5);
+    scene.add(directionalLight4);
+
+    const directionalLight5 = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight5.position.set(5, 0, 0);
+    scene.add(directionalLight5);
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.addEventListener('change', render);
